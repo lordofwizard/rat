7622242fa0d6f8ee56b2592c2677f62418442c09
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-02-05 19:49:42 +0530

    removed all the warnings with the imports and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
-use std::fs::{File, OpenOptions};
+
+use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use colored::Colorize;
@@ -11,14 +11,13 @@ fn main() {
     }
     for i in 2..(args.len()+1){
         let path = Path::new(&args[i-1]);
-        let display = path.display();
         let mut file_string :String = String::new();
         let mut file = match File::open(&path) {
             Err(some) => panic!("{}{}","File Opening failed. Here's Why \n".red(),some),
             Ok(file) => file,
         };
         match file.read_to_string(&mut file_string){
-            Err(some) => panic!("{} here is what happend\n{}","something went wrong while converting the file to string weird shit".red(),some),
+            Err(some) => panic!("{} here is what happened\n{}","something went wrong while converting the file to string weird shit".red(),some),
             Ok(_) => print!("{}",file_string),
         }
     }
